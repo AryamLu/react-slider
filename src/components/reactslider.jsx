75ca9a716d750f03d204694c4ed98973f4f2c0be
Luis <lustaryam@gmail.com>
2023-02-07 22:41:00 -0500

    Apareció la linea

--- src/components/reactslider.jsx
+++ src/components/reactslider.jsx
@@ -6,13 +6,13 @@ export  function Reactslider() {
         <p>b. ¿Por cuántos años?</p>
         <input className='slider1' type="range" min="1" max="7" list='ticks1'/>
         <datalist id="ticks1">
-          <option value="1"></option>
-          <option value="2">5</option>
-          <option value="3">7</option>
-          <option value="4">10</option>
-          <option value="5">13</option>
-          <option value="6">15</option>
-          <option value="7">20</option>
+          <option value="1" class="op1"></option>
+          <option value="2" class="op2">5</option>
+          <option value="3" class="op3">7</option>
+          <option value="4" class="op4">10</option>
+          <option value="5" class="op5">12</option>
+          <option value="6" class="op6">15</option>
+          <option value="7" class="op7">20</option>
         </datalist>
     </div>
     
