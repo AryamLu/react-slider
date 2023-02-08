e867dcb9d11e73b906aecae8048ad67d30801391
jrojavid2 <jrojavid2@outlook.es>
2023-02-07 21:19:29 -0500

    Data list

--- src/components/reactslider.jsx
+++ src/components/reactslider.jsx
@@ -4,16 +4,16 @@ export  function Reactslider() {
   return (
     <div class="slidecontainer">
         <p>b. ¿Por cuántos años?</p>
-        <input className='slider1' type="range" min="1" max="7" />
-        <div class="slider-numbers">
-            <div></div>
-            <div>5</div>
-            <div>7</div>
-            <div>10</div>
-            <div>12</div>
-            <div>15</div>
-            <div>20</div>
-        </div>
+        <input className='slider1' type="range" min="1" max="7" list='ticks1'/>
+        <datalist id="ticks1">
+          <option value="1"></option>
+          <option value="2">5</option>
+          <option value="3">7</option>
+          <option value="4">10</option>
+          <option value="5">13</option>
+          <option value="6">15</option>
+          <option value="7">20</option>
+        </datalist>
     </div>
     
   )
