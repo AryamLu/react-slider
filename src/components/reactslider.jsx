b7d2368865637a5e805dd403ce2b9b6de627b822
Luis <lustaryam@gmail.com>
2023-02-09 11:48:29 -0500

    Cambios generales de div

--- src/components/reactslider.jsx
+++ src/components/reactslider.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 
 export  function Reactslider() {
   return (
-    <div class="slidecontainer">
+    <div className="slidecontainer">
         <p>b. ¿Por cuántos años?</p>
         <input className='slider1' type="range" min="1" max="7" list='ticks1' defaultValue={1}/>
         <datalist id="ticks1">
-          <option value="1" class="op1"></option>
-          <option value="2" class="op2">5</option>
-          <option value="3" class="op3">7</option>
-          <option value="4" class="op4">10</option>
-          <option value="5" class="op5">12</option>
-          <option value="6" class="op6">15</option>
-          <option value="7" class="op7">20</option>
+          <option value="1" className="op1"></option>
+          <option value="2" className="op2">5</option>
+          <option value="3" className="op3">7</option>
+          <div className="limite">
+              <div className="op4">10</div>
+              <div className="icon-star">*</div>
+          </div>
+          <option value="5" className="op5">12</option>
+          <option value="6" className="op6">15</option>
+          <option value="7" className="op7">20</option>
         </datalist>
     </div>
     
