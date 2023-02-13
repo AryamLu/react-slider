9ac8a43231f240bff5fbc65db332e2494b8a53ee
Luis <lustaryam@gmail.com>
2023-02-13 13:42:29 -0500

    llave 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
 import { App } from './App';
-import { ReactDOM } from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
