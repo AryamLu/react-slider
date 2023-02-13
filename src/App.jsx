5bd363e63f63af1208e4f3fc2f83ca6b34c9640a
Luis <lustaryam@gmail.com>
2023-02-13 13:25:41 -0500

    Es la Llave

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import {Reactslider} from './components/reactslider'
+
+export function App() {
+  return (<Reactslider/>
+  )
+}
---
+++ src/components/reactslider.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export function Reactslider() {
+  return (
+    <div className='curly-braces'>
+      <span className='curly-braces-text'>Texto cualquiera</span>
+      <svg className="curly-braces-svg" viewBox={`0 0 100 16`} xmlns="http://www.w3.org/2000/svg">
+      <path
+        d={`M 1 2
+        L 1 7
+        Q 1 10 4 10
+        L 48 10
+        L 50 15
+        L 52 10
+        L 96 10
+        Q 99 10 99 7
+        L 99 2`} stroke="blue" fill="transparent" strokeWidth={1}/>
+      </svg>
+    </div>
+    
+  )
+}
